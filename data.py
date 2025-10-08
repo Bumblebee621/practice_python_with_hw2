8246b447fb37f10fd43edb154292440109c19a59
Bumblebee621 <ariel25621@gmail.com>
2025-10-08 12:27:59 +0300

    finished data_analysis

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import datetime as dt
 
@@ -9,11 +10,15 @@ def add_new_columns(df):
     #2
     dic = {0: "spring", 1: "summer", 2: "fall", 3: "winter"}
     df["season_name"] = df["season"].apply(lambda x: dic[x])
+
     #3
-    df["Hour"] = df["timestamp"].apply(lambda x: dt.datetime.fromtimestamp(x).hour)
-    df["Day"] = df["timestamp"].apply(lambda x: dt.datetime.fromtimestamp(x).day)
-    df["Month"] = df["timestamp"].apply(lambda x: dt.datetime.fromtimestamp(x).month)
-    df["Year"] = df["timestamp"].apply(lambda x: dt.datetime.fromtimestamp(x).year)
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="%d/%m/%Y %H:%M")
+    # now extract hour, date parts
+    df["Hour"] = df["timestamp"].dt.hour
+    df["Day"] = df["timestamp"].dt.day
+    df["Month"] = df["timestamp"].dt.month
+    df["Year"] = df["timestamp"].dt.year
+
     #4
     df["is_weekend_holiday"] = 1 + df["is_weekend"].apply(lambda x: 1 if x else 0) + 2* df["is_holiday"].apply(lambda x: 1 if x else 0)
     #5
@@ -30,9 +35,21 @@ def data_analysis(df):
     print(corr.to_string())
     print()
     #7
-    corrdict =
+    abs_corr = corr.abs()
 
-    for i in range(len(corr)):
-        corrdict = {(corr[i])}
-    corr = corr.sort_values(ascending=False)
-    print("corr output:")
+    mask = np.triu(np.ones(abs_corr.shape, dtype=bool), k= 1)
+    pairs_df = abs_corr.where(mask)
+    corr_dict = {
+        (row, col): pairs_df.loc[row, col]
+        for row in pairs_df.index
+        for col in pairs_df.columns
+        if not pd.isna(pairs_df.loc[row,col])
+    }
+    top5 = sorted(corr_dict.items(), key=lambda x: x[1], reverse=True)[:5]
+    bottom5 = sorted(corr_dict.items(), key=lambda x: x[1])[:5]
+    print("Highest correlated are: ")
+    for ind, tuple in enumerate(top5):
+        print(f"{ind+1}. {tuple[0]} with {tuple[1]:.6f}")
+    print("Lowest correlated are: ")
+    for ind, tuple in enumerate(bottom5):
+        print(f"{ind+1}. {tuple[0]} with {tuple[1]:.6f}")
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from data import *
+from clustering import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+        data = load_data("london_sample_500.csv")
+        data = add_new_columns(data)
+        data = data_analysis(data)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
